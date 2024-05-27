85cd6f860d94facc56612eec0c672449e2a397d2
karangula123 <abhinayavamsi568@gmail.com>
2024-05-27 12:30:53 +0530

    my first react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+
+const App = () => {
+  return (
+    <div>
+      this is react.
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+
+export default App
